9a6d45c3b65287b0e5bdba1942519b229c9c706d
Xrayya <nur.alkhiyar@gmail.com>
2024-06-03 17:52:56 +0700

    refactor(routes.user): extract auth to separarate file

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,3 @@
-import { createUser, loginUser } from "@/controllers/user.controller";
 import authMiddleware from "@/middlewares/auth.middleware";
 import { getLeaderboard, getUserProfile } from "@/controllers/user.controller";
 import express from "express";
@@ -6,8 +5,6 @@ import express from "express";
 const userRouter = express.Router();
 
 userRouter.get("/", (_req, res) => res.json({ message: "Hello, user!" }));
-userRouter.post("/register", createUser);
-userRouter.post("/login", loginUser);
 
 // TODO: this is dummy route, remove it later
 userRouter.delete("/delete", authMiddleware, (_req, res) =>
