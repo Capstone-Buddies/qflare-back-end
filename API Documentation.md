8683e877bf830815343a0674436e6631514bf948
alfian-clouudd <13020210200@umi.ac.id>
2024-06-05 03:11:57 +0800

    fix: update status /register & /login api

--- API Documentation.md
+++ API Documentation.md
@@ -39,10 +39,29 @@ This API documentation provides information on how to interact with the Qflare A
       - `data`:
         - `username` (string): The username of the user
         - `email` (string): The email of the user
+  - **Bad Request: Missing username**:
+    - **Description**: Register a new user with missing username on request body
+    - **Status Code**: `400 Bad Request`
+    - **Response Body**:
+      - `status`: `fail`
+      - `message`: `Username is required`
+  - **Bad Request: Missing email**:
+    - **Description**: Register a new user with missing email on request body
+    - **Status Code**: `400 Bad Request`
+    - **Response Body**:
+      - `status`: `fail`
+      - `message`: `Email is required`
+  - **Bad Request: Missing password**:
+    - **Description**: Register a new user with missing password
+    - **Status Code**: `400 Bad Request`
+    - **Response Body**:
+      - `status`: `fail`
+      - `message`: `Password is required`
   - **Conflict: Email already used**
     - **Description**: Register a new user with email that is already in use
-    - **Status Code**: `403 Forbidden`
+    - **Status Code**: `409 Conflict`
     - **Response Body**:
+      - `status`: `fail`
       - `message`: `Email is already in use`
 
 #### 2.1.2. Login
@@ -63,8 +82,9 @@ This API documentation provides information on how to interact with the Qflare A
       - `token` (string): The JWT token
   - **Unauthorized: Wrong credential**:
     - **Description**: Login with invalid email or password
-    - **Status Code**: `403 Forbidden`
+    - **Status Code**: `401 Unauthorized`
     - **Response Body**:
+      - `status`: `fail`
       - `message`: `Invalid email or password`
 
 ## 3. Other Error
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,7 +4,11 @@ import { findUserByEmail, addUser, getUserByEmail } from "@/models/user.model";
 import { generateToken } from "@/models/auth.model";
 import { LoginRequest, RegisterRequest } from "@/zod/schemas/authRoute";
 
-const success: string = "success";
+
+namespace status {
+  export const success: string = "success";
+  export const fail: string = "fail";
+}
 
 export const register = async (req: RegisterRequest, res: Response) => {
   const { username, email, password } = req.body;
@@ -13,7 +17,8 @@ export const register = async (req: RegisterRequest, res: Response) => {
     const existingUser = await findUserByEmail(email);
 
     if (existingUser.length > 0) {
-      return res.status(403).json({
+      return res.status(409).json({
+        status: status.fail,
         message: "Email is already in use",
       });
     }
@@ -24,26 +29,29 @@ export const register = async (req: RegisterRequest, res: Response) => {
     res.cookie("token", token, {});
 
     return res.status(201).json({
-      status: success,
+      status: status.success,
       data: { username, email },
       message: "User registered successfully",
     });
   } catch (error) {
     console.error("Error creating user:", error);
-    return res.status(400).json({ message: "Unable to add user" });
+    return res
+      .status(400)
+      .json({ status: status.fail, message: "Unable to add user" });
   }
 };
 
 //Login User
 export const login = async (req: LoginRequest, res: Response) => {
-  const { email, password }= req.body;
+  const { email, password } = req.body;
 
   try {
     const result = await getUserByEmail(email);
 
     if (result.length === 0) {
-      return res.status(403).json({
-        message: "Invalid email or password",
+      return res.status(401).json({
+        status: status.fail,
+        message: "Email or password is wrong",
       });
     }
 
@@ -51,21 +59,24 @@ export const login = async (req: LoginRequest, res: Response) => {
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-      return res.status(403).json({
-        message: "Invalid email or password",
+      return res.status(401).json({
+        status: status.fail,
+        message: "Email or password is wrong",
       });
     }
 
     const token = generateToken(user.id, email);
 
     return res.status(200).json({
-      status: success,
+      status: status.success,
       data: { email, token },
       message: "User logged in successfully",
     });
   } catch (error) {
     console.error("Error logging in:", error);
-    return res.status(400).json({ message: "Unable to login" });
+    return res
+      .status(400)
+      .json({ status: status.fail, message: "Unable to login" });
   }
 };
 
