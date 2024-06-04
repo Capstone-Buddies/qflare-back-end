df732c45c76784937c77cdcf05c28f31f6771502
Xrayya <nur.alkhiyar@gmail.com>
2024-06-04 12:22:39 +0700

    refactor(routes.quiz, routes.user): call authMiddleware once

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,12 +4,14 @@ import express from "express";
 
 const userRouter = express.Router();
 
+userRouter.use(authMiddleware);
+
 // TODO: this is dummy route, remove it later
-userRouter.delete("/delete", authMiddleware, (_req, res) =>
-  res.json({ message: "Hello, user!" })
+userRouter.delete("/delete", (_req, res) =>
+  res.json({ message: "Hello, user!" }),
 );
-userRouter.get("/my-profile", authMiddleware, getUserProfile);
-userRouter.get("/leaderboard", authMiddleware, getLeaderboard);
+userRouter.get("/my-profile", getUserProfile);
+userRouter.get("/leaderboard", getLeaderboard);
 
 // NOTE: Optional endpoints, develop later if have more time
 // userRouter.put("/my-profile", (_req, res) => res.json({ message: "Hello, user!" }));
