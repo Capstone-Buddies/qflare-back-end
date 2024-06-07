ff92e45d9b49ffc6194b97337aa55f784b6b0a8a
Xrayya <nur.alkhiyar@gmail.com>
2024-06-07 19:15:52 +0700

    chore(dummy_data): add quiz histories dummy

--- dummy_data/quizHistories.ts
+++ dummy_data/quizHistories.ts
@@ -1,5 +1,6 @@
 import { QuizHistoryType } from "@/drizzle/schema";
 import { QuizCategoryDummyType } from "./quizCategories";
+import { usersDummy } from "./users";
 
 const dummyId = Array.from({ length: 2 }, (_, index) => index + 1);
 
@@ -8,4 +9,19 @@ export type QuizHistoryDummyType = QuizHistoryType & {
   quizCategoryId: QuizCategoryDummyType["id"];
 };
 
-export const quizHistories: QuizHistoryDummyType[] = [];
+export const quizHistories: Omit<QuizHistoryDummyType, "id">[] = [
+  {
+    userId: usersDummy[3].id,
+    timestamp: new Date("2024-05-28T08:00:00Z"),
+    grade: 80,
+    level: 1,
+    quizCategoryId: 4
+  },
+  {
+    userId: usersDummy[4].id,
+    timestamp: new Date("2024-06-13T03:44:00Z"),
+    grade: 90,
+    level: 1,
+    quizCategoryId: 7
+  }
+];
