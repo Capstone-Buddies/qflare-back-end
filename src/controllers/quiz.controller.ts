9593ef14b90ff1ec64a15b1a5d31266de10060b3
Xrayya <nur.alkhiyar@gmail.com>
2024-06-11 18:46:56 +0700

    feat: implement `/quizzes/histories/:historyId/answers`

--- src/controllers/quiz.controller.ts
+++ src/controllers/quiz.controller.ts
@@ -1,6 +1,10 @@
 import { status } from "@/constants";
 import { AuthenticatedRequest } from "@/middlewares/auth.middleware";
-import { getUserQuizHistories } from "@/models/quiz.model";
+import {
+  getQuizHistoryAnswers,
+  getUserQuizHistories,
+} from "@/models/quiz.model";
+import { GetQuizAnswersRequest } from "@/zod/schemas/quizRoute";
 import { Request, Response } from "express";
 
 export const generateQuiz = async (req: Request, res: Response) => {
@@ -25,27 +29,25 @@ export const calculateQuiz = async (req: Request, res: Response) => {
 
 export const getQuizHistories = async (
   req: AuthenticatedRequest,
-  res: Response
+  res: Response,
 ) => {
   try {
-    const userId = req.user?.id;
+    const userId = req.user!.id;
 
-    if (userId) {
-      const histories = await getUserQuizHistories(userId);
+    const histories = await getUserQuizHistories(userId);
 
-      if (histories.length === 0) {
-        return res.status(200).json({
-          status: status.success,
-          message: "User has no quiz histories",
-        });
-      }
-
+    if (histories.length === 0) {
       return res.status(200).json({
         status: status.success,
-        data: { histories },
-        message: "Successfully retrieved quiz histories",
+        message: "User has no quiz histories",
       });
     }
+
+    return res.status(200).json({
+      status: status.success,
+      data: { histories },
+      message: "Successfully retrieved quiz histories",
+    });
   } catch (error) {
     return res.status(500).json({
       status: status.fail,
@@ -54,12 +56,24 @@ export const getQuizHistories = async (
   }
 };
 
-export const getQuizAnswers = async (req: Request, res: Response) => {
-  // TODO: Implement getQuizAnswers
-  return res
-    .json({
+export const getQuizAnswers = async (
+  req: GetQuizAnswersRequest,
+  res: Response,
+) => {
+  const userId = req.user!.id;
+  const historyId: number = parseInt(req.params.historyId);
+
+  try {
+    const answers = await getQuizHistoryAnswers(historyId, userId);
+    return res.status(200).json({
       status: status.success,
-      message: "This endpoint has not implemented yet",
-    })
-    .status(200);
+      data: { answers },
+      message: "Successfully retrieved quiz answers",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: status.fail,
+      message: "An error occurred while load quiz answers",
+    });
+  }
 };
--- src/models/quiz.model.ts
+++ src/models/quiz.model.ts
@@ -1,11 +1,17 @@
 import { db } from "@/drizzle/db";
-import { eq } from "drizzle-orm";
-import { quizHistories, quizCategories } from "@/drizzle/schema";
+import { and, eq } from "drizzle-orm";
+import {
+  quizHistories,
+  quizCategories,
+  answerHistories,
+  quizQuestions,
+} from "@/drizzle/schema";
 
 export const getUserQuizHistories = async (userId: string) => {
   try {
     const histories = await db
       .select({
+        id: quizHistories.id,
         timestamp: quizHistories.timestamp,
         grade: quizHistories.grade,
         level: quizHistories.level,
@@ -14,7 +20,7 @@ export const getUserQuizHistories = async (userId: string) => {
       .from(quizHistories)
       .innerJoin(
         quizCategories,
-        eq(quizHistories.quizCategoryId, quizCategories.id)
+        eq(quizHistories.quizCategoryId, quizCategories.id),
       )
       .where(eq(quizHistories.userId, userId));
 
@@ -23,4 +29,40 @@ export const getUserQuizHistories = async (userId: string) => {
     console.error("Error fetching quiz histories from database:", error);
     throw new Error("Database query failed");
   }
+};
+
+export const getQuizHistoryAnswers = async (
+  historyId: number,
+  userId: string,
+) => {
+  const history = await db
+    .select({
+      question: quizQuestions.question,
+      option1: quizQuestions.option1,
+      option2: quizQuestions.option2,
+      option3: quizQuestions.option3,
+      option4: quizQuestions.option4,
+      userAnswer: answerHistories.userAnswer,
+      correctness: answerHistories.correctness,
+      duration: answerHistories.duration,
+    })
+    .from(answerHistories)
+    .innerJoin(quizQuestions, eq(quizQuestions.id, answerHistories.questionId))
+    .innerJoin(
+      quizHistories,
+      eq(answerHistories.quizHistoryId, quizHistories.id),
+    )
+    .where(
+      and(
+        eq(answerHistories.quizHistoryId, historyId),
+        eq(quizHistories.userId, userId),
+      ),
+    );
+
+  return history.map(({ correctness, ...rest }) => {
+    return {
+      ...rest,
+      correctness: correctness === 1 ? true : false,
+    };
+  });
 };
