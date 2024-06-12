65afc113c093f2a5d5ad6e8fc352e881f16c351c
alfian-clouudd <13020210200@umi.ac.id>
2024-06-12 21:23:45 +0800

    feat: add user leaderboard functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,30 +1,32 @@
 import { status } from "@/constants";
-import { AuthenticatedRequest } from "@/middlewares/auth.middleware";
+import { getLeaderboardQuery } from "@/models/user.model";
 import { Request, Response } from "express";
 
-export const getUserProfile = async (
-  req: AuthenticatedRequest,
-  res: Response,
-) => {
-  const { username, email, level, exp, schoolOrigin, profileImgUrl } = req.user!;
-
-  return res
-    .json({
-      status: status.success,
-      message: "Successfully retrieved user profile",
-      data: { username, email, schoolOrigin, level, exp, profileImgUrl },
-    })
-    .status(200);
-};
-
-export const getLeaderboard = async (req: Request, res: Response) => {
-  // TODO: Implement getLeaderboard
+export const getUserProfile = async (req: Request, res: Response) => {
+  // TODO: Implement getUserProfile
   return res
     .json({
       status: status.success,
       message: "This endpoint has not implemented yet",
     })
     .status(200);
+};
+
+export const getLeaderboard = async (req: Request, res: Response) => {
+  try {
+    const leaderboard = await getLeaderboardQuery();
+
+    return res.status(200).json({
+      status: status.success,
+      data: { leaderboard },
+      message: "Leaderboard fetched successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: status.fail,
+      message: "An error occurred while load Leaderboard",
+    });
+  }
 };
 
 // NOTE: Controllers for optional  endpoints, develop later if have more time
