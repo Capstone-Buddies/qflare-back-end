3ac037751aa1024db0b00922ebbb3c44875e2a2c
alfian-clouudd <13020210200@umi.ac.id>
2024-06-12 21:24:27 +0800

    feat(seed): add profileImgUrl to user seed data

--- src/drizzle/seeder.ts
+++ src/drizzle/seeder.ts
@@ -24,6 +24,7 @@ async function seedUsers() {
       email: user.email,
       password: hashedPassword,
       schoolOrigin: user.schoolOrigin,
+      profileImgUrl: user.profileImgUrl,
     });
   }
 }
