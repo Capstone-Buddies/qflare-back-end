2c520e4cac2bfcc504414d2e4f67d1fa03b786c6
alfian-clouudd <13020210200@umi.ac.id>
2024-06-13 18:52:21 +0800

    fix(user controller): resolve conflict in /getUserProfile endpoint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,14 +1,19 @@
 import { status } from "@/constants";
 import { AuthenticatedRequest } from "@/middlewares/auth.middleware";
 import { getLeaderboardQuery } from "@/models/user.model";
-import { Request, Response } from "express";
+import { Response } from "express";
 
-export const getUserProfile = async (req: Request, res: Response) => {
-  // TODO: Implement getUserProfile
+export const getUserProfile = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const { username, email, level, exp, schoolOrigin, profileImgUrl } =
+    req.user!;
   return res
     .json({
       status: status.success,
-      message: "This endpoint has not implemented yet",
+      message: "Successfully retrieved user profile",
+      data: { username, email, schoolOrigin, level, exp, profileImgUrl },
     })
     .status(200);
 };
